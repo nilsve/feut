9870ff5a0104574613f0b1f740a08ecce05e249d
Indy Maat <indy@indymaat.nl>
2018-04-04 15:52:30 +0200

    Branch hernoemd naar dependencies.

--- shared/src/main/java/com/feut/shared/connection/packets/LoginRequest.java
+++ shared/src/main/java/com/feut/shared/connection/packets/LoginRequest.java
@@ -8,4 +8,13 @@ import java.util.List;
 public class LoginRequest extends Packet {
     public String username;
     public String password;
+
+    @Override
+    public String toString() {
+        return "LoginRequest{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", packetType='" + packetType + '\'' +
+                '}';
+    }
 }
