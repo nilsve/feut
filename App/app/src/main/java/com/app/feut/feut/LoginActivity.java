ac0b29d373e6b7a03a0fc3cb2ecca54ef97de010
Indy Maat <indy@indymaat.nl>
2018-04-16 11:52:11 +0200

    Homescreen opent nu na succesvolle login. Registeractivity opent nu vanuit LoginActivity

--- App/app/src/main/java/com/app/feut/feut/LoginActivity.java
+++ App/app/src/main/java/com/app/feut/feut/LoginActivity.java
@@ -1,7 +1,7 @@
 package com.app.feut.feut;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -18,9 +18,11 @@ import com.feut.shared.connection.packets.LoginRequest;
 public class LoginActivity extends AppCompatActivity {
     private TextView usernameText;
     private TextView passwordText;
+    public static boolean loginValid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        loginValid = false;
 
         new Thread(Connection.getInstance()).start(); // TODO: Niet de beste plek hiervoor
 
@@ -33,13 +35,36 @@ public class LoginActivity extends AppCompatActivity {
         Button loginButton = (Button) findViewById(R.id.loginButton);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public synchronized void onClick(View view) {
                 LoginRequest request = new LoginRequest();
                 request.username = usernameText.getText().toString();
                 request.password = passwordText.getText().toString();
 
                 new SendPacketTask().execute(request);
+                try {
+                    this.wait(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (loginValid) {
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    }
+                }
+
+
+            }
+        });
+
+        Button registerButton = (Button) findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(registerIntent);
             }
         });
         }
+
+    public static void setLoginValid() { loginValid = true; }
+
 }
