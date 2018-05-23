ab84015c3c5ee2170d3c3171727e6d16a2673537
Nils van Eijk <nils.van.eijk@blink.nl>
2018-05-23 19:18:29 +0200

    Arduino eerste opzet

---
+++ Arduino/reader.c/reader.c.ino
@@ -0,0 +1,61 @@
+
+/******************************************
+        PURPOSE:  Learn to use the MF522-AN RFID card reader 
+  Created by      Rudy Schlaf for www.makecourse.com
+  DATE:   2/2014
+*******************************************/
+
+/*
+ * This sketch uses the MFRC522 Library to use ARDUINO RFID MODULE KIT 13.56 MHZ WITH TAGS SPI W AND R BY COOQROBOT.
+ * The library file MFRC522.h has a wealth of useful info. Please read it.
+ * The functions are documented in MFRC522.cpp.
+ *
+ * Based on code Dr.Leong   ( WWW.B2CQSHOP.COM )
+ * Created by Miguel Balboa (circuitito.com), Jan, 2012.
+ * Rewritten by Søren Thing Andersen (access.thing.dk), fall of 2013 (Translation to English, refactored, comments, anti collision, cascade levels.)
+ * 
+ * This library has been released into the public domain.
+*/
+
+#include <SPI.h>//include the SPI bus library
+#include <MFRC522.h>//include the RFID reader library
+
+#define SS_PIN 10  //slave select pin
+#define RST_PIN 5  //reset pin
+MFRC522 mfrc522(SS_PIN, RST_PIN);        // instatiate a MFRC522 reader object.
+MFRC522::MIFARE_Key key;//create a MIFARE_Key struct named 'key', which will hold the card information
+
+void setup() {
+  Serial.begin(115200);        // Initialize serial communications with the PC
+  SPI.begin();               // Init SPI bus
+  mfrc522.PCD_Init();        // Init MFRC522 card (in case you wonder what PCD means: proximity coupling device)
+  Serial.println("Scan a MIFARE Classic card");
+  
+  // Prepare the security key for the read and write functions - all six key bytes are set to 0xFF at chip delivery from the factory.
+  // Since the cards in the kit are new and the keys were never defined, they are 0xFF
+  // if we had a card that was programmed by someone else, we would need to know the key to be able to access it. This key would then need to be stored in 'key' instead.
+  
+  for (byte i = 0; i < 6; i++) {
+          key.keyByte[i] = 0xFF;//keyByte is defined in the "MIFARE_Key" 'struct' definition in the .h file of the library
+  }
+
+}
+
+void loop()
+{
+    // Look for new cards (in case you wonder what PICC means: proximity integrated circuit card)
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {//if PICC_IsNewCardPresent returns 1, a new card has been found and we continue
+    return;//if it did not find a new card is returns a '0' and we return to the start of the loop
+  }
+
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) {//if PICC_ReadCardSerial returns 1, the "uid" struct (see MFRC522.h lines 238-45)) contains the ID of the read card.
+    return;//if it returns a '0' something went wrong and we return to the start of the loop
+  }
+
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+    Serial.print(mfrc522.uid.uidByte[i]);
+  }
+  Serial.print("\n");
+}
+
